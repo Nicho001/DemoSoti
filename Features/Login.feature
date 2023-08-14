9a22af21c5f96080203ab06591adea8e65d70316
Nicho001 <nicholassoman01@gmail.com>
2023-08-14 10:07:37 +0530

    First

---
+++ Features/Login.feature
@@ -0,0 +1,9 @@
+﻿Feature: Login
+
+A short summary of the feature
+
+@tag1
+Scenario: login using valid username and password
+	Given entered valid username and password
+	When pressed submit button
+	Then login successfull
---
+++ StepDefinitions/Login.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BDD.StepDefinitions
+{
+    [Binding]
+    internal class Login
+    {
+        [Given(@"entered valid username and password")]
+        public void GivenEnteredValidUsernameAndPassword()
+        {
+            Console.WriteLine("Valid username n password");
+        }
+
+        [When(@"pressed submit button")]
+        public void WhenPressedSubmitButton()
+        {
+            Console.WriteLine("Pressed submit button");
+        }
+
+        [Then(@"login successfull")]
+        public void ThenLoginSuccessfull()
+        {
+            Console.WriteLine("Login is successfull");
+        }
+
+    }
+}
